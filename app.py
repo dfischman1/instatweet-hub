0a47f0b7acf636867b5e33dcee4e99481d9515d8
Daelin Fischman <dfischman1@gmail.com>
2013-04-14 00:38:52 -0400

    flask

--- app.py
+++ app.py
@@ -3,7 +3,6 @@ from flask import request
 from flask import render_template
 from flask import url_for,redirect,flash
 from flask import session, escape
-import db
 
 app = Flask(__name__)
 
@@ -16,4 +15,5 @@ app.debug=True
 def default():
       return render_template('index.html')
 
-
+if __name__ == '__main__':
+    app.run()
