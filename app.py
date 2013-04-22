97cf05ddcd7b3398ff4b55abd489158c853eb4bf
Daelin Fischman <dfischman1@gmail.com>
2013-04-21 21:54:50 -0400

    register

---
+++ app.py
@@ -0,0 +1,65 @@
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask import url_for,redirect,flash
+from flask import session, escape
+#import requests
+#import twyth
+#import tweets
+import storage
+db = storage.db()
+
+app = Flask(__name__)
+
+
+app.debug=True
+
+
+
+
+
+@app.route('/', methods= ['GET', 'POST'])
+def default():
+    if request.method == 'GET':
+        return render_template('homepage.html')
+    else:
+        button = request.form["Go"]
+        if button == "register":
+            return redirect( url_for("register"))
+        else:
+            results = 2
+            user = request.form['username']
+            password = request.form['password']
+            results = db.validate(user, password)
+            if results == True:
+                return redirect(url_for('http://www.nytimes.com'))
+            else:
+                return render_template('homepage.html', results = results)
+
+
+
+
+@app.route('/register', methods= ['GET'])
+def register():
+    return render_template ('register.html')
+
+        
+
+     
+
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+
+
+import os
+key = os.urandom(24)
+#print key
+app.secret_key = key
+
+
+
+
+if __name__ == '__main__':
+    app.run()
