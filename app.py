d79b6b5b104b156f76ff79d33cfad7ef9ae3c7a9
Patrick So <bassicozz@gmail.com>
2013-04-27 23:18:52 -0400

    register (doesnt work yet)

--- app.py
+++ app.py
@@ -40,10 +40,21 @@ def index():
      
 
 
-@app.route('/register')
+@app.route('/register', methods= ['GET', 'POST'])
 def register():
-    return render_template('register.html')
+    if request.method == 'GET':
+        return render_template('register.html')
+    else:
+        uname = request.form['username']
+        password = request.form['pswd']
+        fullname = request.form['name']
+        tuname = request.form['twitter']
+        storage.addUser(uname, password, fullname, tuname)
+        redirect(url_for('search'))
 
+@app.route('/search')
+def search():
+    return render_template('search.html')
 
 import os
 key = os.urandom(24)
