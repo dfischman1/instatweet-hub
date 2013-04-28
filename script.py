a7ac639b68304a4e7c3badfa281db4d437ac788e
Daniel Teehan <drteehan@gmail.com>
2013-04-27 20:18:21 -0400

    example of how to continuously run

--- script.py
+++ script.py
@@ -1,2 +1,11 @@
 #!/usr/local/bin/python
-print "fischman"
+import threading
+
+
+def continuousUpdate():    
+    print 'fischman'
+    s = threading.Timer(1.0, continuousUpdate)
+    s.start()
+
+if __name__=='__main__':
+    continuousUpdate()
