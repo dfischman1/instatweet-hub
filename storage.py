3e85d827472e9d11721181d8e20d7dbf526fb520
Leopold Spohngellert <leopoldsg94@gmail.com>
2013-04-29 18:05:34 -0400

    twitter

--- storage.py
+++ storage.py
@@ -1,5 +1,5 @@
 from pymongo import Connection
-
+import pythontwitter2.tweets
 
 connection = Connection('mongo2.stuycs.org')
 db=connection.admin
@@ -11,7 +11,7 @@ clct=db.roundtable
 
 def addUser(uname, password, fullname, tuname):
     if len(list(clct.find({'username':uname})))==0:
-        clct.insert({'username':uname, 'password':password, 'full': fullname, 'tuname':tuname})
+        clct.insert({'username':uname, 'password':password, 'full': fullname, 'tuname':tuname, 'tweets': [], 'imgs': []})
         return 1
     else:
         print "username is already taken, try another"
@@ -32,11 +32,33 @@ def changePass(uname, oldpass, newpass):
         clct.update({'username':uname,'password':oldpass},{'username':uname,'password':newpass})
 
 def reset():
-    clct.drop()
+    print clct.drop()
 
 
-addUser('Daniel','dobby','Daniel Teehan', 'teeman')
-#print validate('Daniel', 'dobby')
+def addTweets(uname, hashtag):
+    for post in clct.find({'username': uname}):
+        tuname = post['tuname']
+    matches = pythontwitter2.tweets.get_easy(tuname, hashtag)
+    for x in matches:
+        clct.update({ 'username' : uname }, {'$addToSet': { 'tweets': x} })
+
+def getTweets(uname):
+    x = 0
+    for post in clct.find({'username': uname}):
+        for tweet in post['tweets']:
+            print "Your stored tweets" + tweet
+            x += 1
+    return x
+            
+    
+
+reset()
+addUser('Daniel','dobby','Daniel Teehan', 'leopoldsg94')
+print validate('Daniel', 'dobby')
+
+addTweets('Daniel', 'Mets')
+
+print getTweets('Daniel')
 
 #if __name__=="__main__":
 #    addUser("daniel teehan", 'daelin', 'daelin fisch', "@dfisch")
