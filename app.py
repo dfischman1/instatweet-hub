ce0634cb8fe3bc15dad84963bf9919de66b0bfab
teehan <drteehan@gmail.com>
2013-05-05 02:22:54 -0300

    d

--- app.py
+++ app.py
@@ -124,9 +124,10 @@ def instaregister():
     else:
         btn = request.form['Go']
     if btn == "Instagram":
+        print request.args.keys()
         uname = request.args.get('uname')
         user_hashtag=request.args.get('user_hashtag')
-        queries='?' + 'uname='+uname+'user_hashtag='+user_hashtag
+        queries='?' + 'uname='+uname+'&user_hashtag='+user_hashtag
         api = client.InstagramAPI(client_id='2e1ab1ca522343a589a4dc84eb31af41',
                                   client_secret='67b832ed8a9b4e67b8696a3db0a69fd2',
                                   redirect_uri='http://ml7.stuycs.org:6376/instagram' + queries)
@@ -165,6 +166,7 @@ def instagram():
     #global user_hashtag
     uname = request.args.get('uname')
     user_hashtag = request.args.get('user_hashtag')
+    print request.args.keys()
     print uname
     print user_hashtag
     try:
