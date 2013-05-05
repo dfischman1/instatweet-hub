ef2355cf606be91000ab0b0c608e811a346c2756
teehan <drteehan@gmail.com>
2013-05-05 01:50:34 -0300

    d

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ app.debug=True
 CONFIG = {
     'client_id': '2e1ab1ca522343a589a4dc84eb31af41',
     'client_secret': '67b832ed8a9b4e67b8696a3db0a69fd2',
-    'redirect_uri': (url_for('instaregister', uname=uname, user_hashtag=user_hashtag))
+    'redirect_uri': (url_for('instagram', uname=uname, user_hashtag=user_hashtag))
 }
 
 user_id = 0
